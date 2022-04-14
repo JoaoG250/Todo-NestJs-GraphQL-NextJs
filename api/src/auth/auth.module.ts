d70677171101be3b6d094cdb168308a0ce6e8c77
João Rodrigues <joao.devmail@gmail.com>
2022-04-13 22:40:27 -0300

    feat: auth guards and strategies

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,7 +1,13 @@
 import { Module } from "@nestjs/common";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "src/jwt/jwt.module";
+import { JwtStrategy } from "./strategies/jwt.strategy";
+import { RedisModule } from "src/redis/redis.module";
 import { AuthService } from "./auth.service";
 
 @Module({
-  providers: [AuthService],
+  imports: [PassportModule, JwtModule, RedisModule],
+  providers: [JwtStrategy, AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
---
+++ api/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import config from "config";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable } from "@nestjs/common";
+import { AuthJwtPayload, AuthUser } from "../types";
+
+const accessTokenSecret: string = config.get("jwt.accessTokenSecret");
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: accessTokenSecret,
+    });
+  }
+
+  async validate(payload: AuthJwtPayload): Promise<AuthUser> {
+    return { id: payload.id };
+  }
+}
